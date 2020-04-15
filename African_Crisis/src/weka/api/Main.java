37c22897673fbf882314669e6878b6ca8668a65b
Zeynep <zeynep.danishh@gmail.com>
2020-04-15 22:07:04 +0300

    Zeynep Environment for Knowledge Analysis

--- African_Crisis/src/weka/api/Main.java
+++ African_Crisis/src/weka/api/Main.java
@@ -6,21 +6,7 @@ public class Main {
 
 	public static void main (String[] args) {
 		
-		//Dosyanýn içindeki tüm dosyalar silinip, sadece kalmasý gereken 3 dosya býrakýlmasý kodu
-		//File folder = folder.listFiles; 
-		/*
-		String path1 = "/User/Zeynep/eclipse-workspace/African_Crisis/Documents/african_crises.arff";
-		String path2 = "/User/Zeynep/eclipse-workspace/African_Crisis/Documents/african_crises2.arff";
-		String path3 = "/User/Zeynep/eclipse-workspace/African_Crisis/Documents/african_crises3.arff";
-		String path4 = "/User/Zeynep/eclipse-workspace/African_Crisis/Documents/IBk.model";
-		String path5 = "/User/Zeynep/eclipse-workspace/African_Crisis/Documents/J48.model";
-		String path6 = "/User/Zeynep/eclipse-workspace/African_Crisis/Documents/Logistic.model";
-		String path7 = "/User/Zeynep/eclipse-workspace/African_Crisis/Documents/OneR.model";
-		String path8 = "/User/Zeynep/eclipse-workspace/African_Crisis/Documents/situationActual.arff";
-		String path9 = "/User/Zeynep/eclipse-workspace/African_Crisis/Documents/ZeroR.model";
-		*/	
-		
-		//Ana kullanýcý ekraný açýlýr ve iþlemlere baþlanýr
+		//Ana kullanici ekrani acilir ve islemlere baslanir
 		Window1 window = new Window1();
 		
 	}
